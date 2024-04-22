6434c99e2f2e22adecdac65bec1c6d1219d4d43b
David Gonzalez <davidega12@gmail.com>
2024-04-21 23:38:36 -0600

    Valor de cada carta

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -48,3 +48,25 @@ const perdirCarta = () => {
 
 
 // perdirCarta();
+const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10 
+            : valor * 1;
+    
+    // let puntos = 0;
+    // // 2 = 2 10 = 10, 3 = 3
+    // if ( isNaN( valor ) ){
+        
+    //     puntos = ( valor === 'A' ) ? 11 : 10;
+
+    // }else{
+    //     puntos = valor * 1;
+    // }
+    // console.log(puntos);
+
+}
+
+const valor = valorCarta( perdirCarta() );
+console.log({valor});
